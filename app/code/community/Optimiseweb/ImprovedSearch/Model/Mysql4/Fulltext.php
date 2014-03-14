73c6466a3bd41b8005e193a2d2cbe0ea3a41697a
Kathirvel <sid@kathirvel.com>
2014-03-14 18:07:33 +0000

    New

--- app/code/community/Optimiseweb/ImprovedSearch/Model/Mysql4/Fulltext.php
+++ app/code/community/Optimiseweb/ImprovedSearch/Model/Mysql4/Fulltext.php
@@ -4,7 +4,7 @@
  * Optimiseweb ImprovedSearch Model Mysql4 Fulltext
  *
  * @package     Optimiseweb_ImprovedSearch
- * @author      Sid Vel (sid@optimiseweb.co.uk)
+ * @author      Kathir Vel (sid@optimiseweb.co.uk)
  * @copyright   Copyright (c) 2014 Optimise Web
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
